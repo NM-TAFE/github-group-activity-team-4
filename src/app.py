cbfb7ce2c084d8ed7f06d0884025be7f31b6d9e0
LB-TAFE <161671624+LB-TAFE@users.noreply.github.com>
2024-09-16 12:42:14 +0800

    Merge: Resolve merging issues with main

--- src/app.py
+++ src/app.py
@@ -1,4 +1,3 @@
-from game import Game
 from flask import Flask, render_template, request, redirect, url_for
 from game import Game
 
@@ -11,38 +10,9 @@ app = App()
 
 @app.route('/')
 def index():
-<<<<<<< HEADgit
-    winner = Game.check_winner()
-    draw = Game.check_draw()
-    return render_template('index.html', board=board, current_player=current_player, winner=winner, draw=draw)
-
-
-@app.route('/play/<int:cell>')
-def play(cell):
-    # breakpoint()
-    global current_player
-    if board[cell] == ' ':
-        board[cell] = current_player
-        if not Game.check_winner():
-            current_player = 'O' if current_player == 'X' else 'X'
-    return redirect(url_for('index'))
-
-
-@app.route('/reset')
-def reset():
-    global board, current_player
-    board = [' '] * 9
-    current_player = 'X'
-    return redirect(url_for('index'))
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
     winner = app.game.check_winner()
     draw = app.game.check_draw()
-
-        return render_template('index.html', board=app.game.board, current_player=app.game.current_player, winner=winner, draw=draw)
+    return render_template('index.html', board=app.game.board, current_player=app.game.current_player, winner=winner, draw=draw)
 
 app.route('/play/<int:cell>')(app.game.play)
 app.route('/reset')(app.game.reset)
-
--- tests/test_game.py
+++ tests/test_game.py
@@ -1,4 +1,3 @@
-# PLACEHOLDER SO FOLDER IS DETECTED BY GIT
 import unittest
 from src.game import Game
 from src.app import App
