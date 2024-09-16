13adbb4152aaa11b8063e6ac22edd6e6bd39f569
harpreet <20126185@tafe.wa.edu.au>
2024-09-16 13:02:26 +0800

    issue : test check  for winner function

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,14 +1,12 @@
-<<<<<<< HEAD
+
 import unittest
 from src.game import Game
 from src.app import App
 
 class TestGame(unittest.TestCase):
-    def test_winner_check():
+    def test_winner_check(self):
         game = Game(board=['X', 'X', 'X', ' ', ' ', ' ', ' ', ' ', ' '])
         assert game.check_winner() == 'X'
 
 
-=======
-# PLACEHOLDER SO FOLDER IS DETECTED BY GIT
->>>>>>> main
+
